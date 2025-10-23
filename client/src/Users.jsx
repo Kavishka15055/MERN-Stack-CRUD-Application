4b0c8ab174d67960776e44d58997bb453d52ca54
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-23 23:17:20 +0530

    Update Users.jsx

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from "react-router-dom";
 
 function Users (){
-    const [users,setUsers]=useState
+    const [users,setUsers]=useState([{
+        Name:"yousaf", Email:"ysf@gmail.com", Age: 20
+    }])
     return(
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className="w-50 bg-white rounded p-3">
+                <Link to="/create" className="btn btn-success">Add +</Link>
                 <table className="table">
                     <thead>
                         <tr>
@@ -16,7 +20,19 @@ function Users (){
                         </tr>
                     </thead>
                     <tbody>
-
+                        {
+                            users.map((user) =>{
+                                return<tr>
+                                    <td>{user.Name}</td>
+                                    <td>{user.Email}</td>
+                                    <td>{user.Age}</td>
+                                    <td>
+                                        <Link to="/update" className="btn btn-success">Update</Link>
+                                        <button>Delete</button>
+                                    </td>
+                                </tr>
+                            })
+                        }
                     </tbody>
                 </table>
             </div>
