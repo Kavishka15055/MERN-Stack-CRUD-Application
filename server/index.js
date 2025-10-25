58a3608dc618419057122a573a42f24eac33a511
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-25 13:35:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,15 +8,21 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/crud")
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.log("❌ MongoDB connection error:", err));
 
-app.post("/createUser", (req, res) => {
-  UserModel.create(req.body)
+app.get('/', (req, res) => {
+  UserModel.find({})
     .then(users => res.json(users))
     .catch(err => res.status(500).json(err));
 });
 
+app.post("/createUser", (req, res) => {
+  UserModel.create(req.body)
+    .then(user => res.json(user))
+    .catch(err => res.status(500).json(err));
+});
+
 app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+  console.log("🚀 Server is running on port 3001");
 });
