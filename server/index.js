fda7b215a781c8f2aa9fbd1d8e23c4ea60439e8d
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-25 13:00:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel = require('./models/Users');
 
-const app = express ()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(3001, () =>{
-    console.log("Server is Running")
-})
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.post("/createUser", (req, res) => {
+  UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.status(500).json(err));
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
